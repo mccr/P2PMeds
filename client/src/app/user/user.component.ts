f235ca7d7274746137189fcb259ad887508ba75a
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-08-05 22:02:09 +0200

    profile view

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -11,7 +11,7 @@ import "rxjs/add/operator/mergeMap";
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
+  styleUrls: ['./user.component.min.css'],
   providers: [UserService]
 })
 export class UserComponent implements OnInit {
